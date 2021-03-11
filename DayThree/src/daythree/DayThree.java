78550be50bb708e609747cb80a5f592ed4e5e772
Damian Zylski <damian.zylski@smoothstack.com>
2021-03-11 01:16:11 -0600

    Start day 3

---
+++ DayThree/src/daythree/DayThree.java
@@ -0,0 +1,45 @@
+package daythree;
+/*
+ * Programmer: Damian Zylski
+ * Project:    TrianglesFromNotes
+ * Date:       03/10/2021
+ * System:     Windows 7 Enterprise - Netbeans 12
+ * 
+ * Purpose:    To let the user provide a filepath and display all files and folders
+ * in said directory. This program will also allow text to be appended to a file and will
+ * have the ability to search for a particular character in a file and count it.
+ */
+
+import java.util.Scanner;
+
+public class DayThree
+{
+    
+//*directoryChecker*********************************************************************************
+    //Lists the files and folders in a directory
+    public static void directoryChecker()
+    {
+        
+    }
+    
+//*textAppender*********************************************************************************
+    //Appends text to a file
+    public static void textAppender()
+    {
+        
+    }
+    
+//*charSearch*********************************************************************************
+    //Searches for a specific char in a file and counts it
+    public static void charSearch()
+    {
+        
+    }
+    
+//*main*********************************************************************************
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
