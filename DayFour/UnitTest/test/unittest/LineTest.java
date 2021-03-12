983f63f9b0bd2e70e7c38c1bf65ec516f35fcf25
Damian Zylski <damian.zylski@smoothstack.com>
2021-03-12 14:06:53 -0600

    Revert to junit4

--- DayFour/UnitTest/test/unittest/LineTest.java
+++ DayFour/UnitTest/test/unittest/LineTest.java
@@ -1,18 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package unittest;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
  * @author Zylski
- * LineTest
- * 3/12/21
- * Unit test for line class
  */
 public class LineTest
 {
@@ -21,22 +23,22 @@ public class LineTest
     {
     }
     
-    @BeforeAll
+    @BeforeClass
     public static void setUpClass()
     {
     }
     
-    @AfterAll
+    @AfterClass
     public static void tearDownClass()
     {
     }
     
-    @BeforeEach
+    @Before
     public void setUp()
     {
     }
     
-    @AfterEach
+    @After
     public void tearDown()
     {
     }
