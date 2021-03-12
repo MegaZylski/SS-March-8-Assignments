7d0695af01423fc39f1ec28de910e554f8bd7688
Damian Zylski <damian.zylski@smoothstack.com>
2021-03-12 02:43:01 -0600

    Update

---
+++ DayFour/UnitTest/src/unittest/UnitTest.java
@@ -0,0 +1,17 @@
+package unittest;
+/*
+ * Programmer: Damian Zylski
+ * Project:    UnitTest
+ * Date:       03/11/2021
+ * System:     Windows 7 Enterprise - Netbeans 12
+ * 
+ * Purpose:    To perform unit testing on a line class
+ */
+public class UnitTest
+{
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
