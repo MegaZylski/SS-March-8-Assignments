1a307a97607037011e949c2d8db48ffcfb047acf
Damian Zylski <damian.zylski@smoothstack.com>
2021-03-17 16:24:07 -0500

    Maven project added

--- Week two/DayOne/Maven/src/main/java/com/mycompany/maven/LoginServlet.java
+++ Week two/DayOne/Maven/src/main/java/com/mycompany/maven/LoginServlet.java
@@ -1,4 +1,9 @@
-package loginservlet;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.maven;
 
 import java.io.IOException;
 import java.io.PrintWriter;
